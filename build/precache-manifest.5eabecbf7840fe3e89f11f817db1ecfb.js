5eb94d0def761df83a84d5031e0aff45b403c039
Nathan Tarasiuk <ntarasiuk@gmail.com>
2020-04-14 16:27:56 -0700

    added "/build"

---
+++ build/precache-manifest.5eabecbf7840fe3e89f11f817db1ecfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e81449663cab107b705d9e5e0c6f709",
+    "url": "/cbc-equipping-classes/build/index.html"
+  },
+  {
+    "revision": "4cf87f377637925c19ba",
+    "url": "/cbc-equipping-classes/build/static/css/main.ce9f0923.chunk.css"
+  },
+  {
+    "revision": "80493131e42f65ebfef2",
+    "url": "/cbc-equipping-classes/build/static/js/2.46dec348.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/cbc-equipping-classes/build/static/js/2.46dec348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf87f377637925c19ba",
+    "url": "/cbc-equipping-classes/build/static/js/main.e43a327c.chunk.js"
+  },
+  {
+    "revision": "67fda7e587494e6c725b",
+    "url": "/cbc-equipping-classes/build/static/js/runtime-main.d505b795.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cbc-equipping-classes/precache-manifest.83dc1435e3626be3d7443c717cdbcb41.js"
+  "/cbc-equipping-classes/build/precache-manifest.5eabecbf7840fe3e89f11f817db1ecfb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cbc-equipping-classes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cbc-equipping-classes/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
