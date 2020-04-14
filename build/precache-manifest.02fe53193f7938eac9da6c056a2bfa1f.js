9d183fc388165bceb792f39821acb89745f40a2a
Nathan Tarasiuk <ntarasiuk@gmail.com>
2020-04-14 16:15:13 -0700

    added build

---
+++ build/precache-manifest.02fe53193f7938eac9da6c056a2bfa1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34929e65d1da0ce93081840321e37b5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace66af3382f71fc336f",
+    "url": "/static/css/main.ce9f0923.chunk.css"
+  },
+  {
+    "revision": "80493131e42f65ebfef2",
+    "url": "/static/js/2.46dec348.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.46dec348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace66af3382f71fc336f",
+    "url": "/static/js/main.cb9f5c34.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
