5c3c59ff860ec8ebd8e952fbee2da5d4dd24e60b
Nathan Tarasiuk <ntarasiuk@gmail.com>
2020-04-14 17:57:22 -0700

    Updates

---
+++ precache-manifest.76d8d8d98ae5257504a7b877cfb0f989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37a2647e33d2ef657b549bd5bc6fd1c",
+    "url": "/cbc-equipping-classes/index.html"
+  },
+  {
+    "revision": "09859bf7ad8dd28b0136",
+    "url": "/cbc-equipping-classes/static/css/main.ce9f0923.chunk.css"
+  },
+  {
+    "revision": "dbb338892ff7f09f9f0a",
+    "url": "/cbc-equipping-classes/static/js/2.bcad07b5.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/cbc-equipping-classes/static/js/2.bcad07b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09859bf7ad8dd28b0136",
+    "url": "/cbc-equipping-classes/static/js/main.7ec12930.chunk.js"
+  },
+  {
+    "revision": "ee15587937e159c69e92",
+    "url": "/cbc-equipping-classes/static/js/runtime-main.e9eb58bf.js"
+  }
+]);
