5558797b79378a89cdb0fb97a68bbd265bcfc815
Nathan Tarasiuk <ntarasiuk@gmail.com>
2020-05-18 10:29:09 -0700

    updated deployment

--- build/precache-manifest.620c43400834b28992d06b0e431c9785.js
+++ build/precache-manifest.620c43400834b28992d06b0e431c9785.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9531173062a763ebdfef77838965b11",
+    "revision": "9eb83480a5b94bce91c8b37b8337f4f3",
     "url": "/cbc-equipping-classes/index.html"
   },
   {
-    "revision": "d49c98f9e46be689448e",
+    "revision": "521094ad9b05729b0d44",
     "url": "/cbc-equipping-classes/static/css/main.ce9f0923.chunk.css"
   },
   {
-    "revision": "215968432ce30a75ac1b",
-    "url": "/cbc-equipping-classes/static/js/2.7790f054.chunk.js"
+    "revision": "7f371dc68848b2135fb2",
+    "url": "/cbc-equipping-classes/static/js/2.6aa5bfb8.chunk.js"
   },
   {
     "revision": "64bea2e190f27355a76963b8cb94bb7b",
-    "url": "/cbc-equipping-classes/static/js/2.7790f054.chunk.js.LICENSE.txt"
+    "url": "/cbc-equipping-classes/static/js/2.6aa5bfb8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d49c98f9e46be689448e",
-    "url": "/cbc-equipping-classes/static/js/main.ee486088.chunk.js"
+    "revision": "521094ad9b05729b0d44",
+    "url": "/cbc-equipping-classes/static/js/main.315dce8c.chunk.js"
   },
   {
     "revision": "ee15587937e159c69e92",
