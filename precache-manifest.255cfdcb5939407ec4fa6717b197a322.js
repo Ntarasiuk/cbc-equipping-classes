84a221e852bfedb81411eb6b842cb06d24320b94
ntarasiuk@gmail.com <nathan@replicant.media>
2021-09-30 19:21:27 -0700

    Initial commit

---
+++ precache-manifest.255cfdcb5939407ec4fa6717b197a322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c155aea1def2e77fc36db238193eaeab",
+    "url": "/cbc-equipping-classes/index.html"
+  },
+  {
+    "revision": "a0d598bdf8811790ac78",
+    "url": "/cbc-equipping-classes/static/css/main.ce9f0923.chunk.css"
+  },
+  {
+    "revision": "d2e52c511e24441c3c5f",
+    "url": "/cbc-equipping-classes/static/js/2.8a47f6b4.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/cbc-equipping-classes/static/js/2.8a47f6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d598bdf8811790ac78",
+    "url": "/cbc-equipping-classes/static/js/main.b17242b9.chunk.js"
+  },
+  {
+    "revision": "ee15587937e159c69e92",
+    "url": "/cbc-equipping-classes/static/js/runtime-main.e9eb58bf.js"
+  }
+]);
